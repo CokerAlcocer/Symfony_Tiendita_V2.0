cb7645c7f644b0a2e9bffec67a17c1fadc6c5526
AngelAlcocerDuran <20203tn066utez.edu.mx>
2021-10-29 09:13:01 -0500

    UPDATE 29/10/2021 Se añadieron nuevos modals y funciones

--- client/js/productController.js
+++ client/js/productController.js
@@ -29,8 +29,8 @@ const getProducts = async() =>{
             +"<td>"+listProducts[i].precio+ "</td>"
             +"<td>"+listProducts[i].status + "</td>"
             +"<td class='text-center'><button class='btn btn-primary' data-toggle='modal' data-target='#details' onclick='getDetailsById("+listProducts[i].idproduct+")'><i class='fas fa-info-circle'></i></button></td>"
-            +"<td class='text-center'><button class='btn btn-warning'  onclick='getById("+listProducts[i].idproduct+")'><i class='far fa-edit'></i></button></td>"
-            +"<td class='text-center'><button class='btn btn-danger' onclick='remove("+listProducts[i].idproduct+")'><i class='far fa-trash-alt'></i></button></td>"
+            +"<td class='text-center'><button class='btn btn-warning' data-toggle='modal' data-target='#update' onclick='getInfoUpdate("+listProducts[i].idproduct+")'><i class='far fa-edit'></i></button></td>"
+            +"<td class='text-center'><button class='btn btn-danger' data-toggle='modal' data-target='#delete' onclick='remove("+listProducts[i].idproduct+")'><i class='far fa-trash-alt'></i></button></td>"
             +"</tr>")
         }
     })
@@ -50,17 +50,47 @@ const getDetailsById = async (id) => {
     let status = document.getElementById('status').value = product.product[0].status ? "Activo" : "Inactivo";
 }
 
-const create = () => {
-    let name = document.getElementById("name").value;
-    let precio = document.getElementById("precio").value;
+const create = async () => {
+    let name = document.getElementById("c_name").value;
+    let precio = document.getElementById("c_precio").value;
 
-    let object = {name, precio};
-    console.log(object);
-
-    $.ajax({
+    await $.ajax({
         type: 'POST',
         url: url+'/product/create',
-        data: object
+        data: {name, precio}
+    }).done(res => {
+        console.log(res);
+    });
+}
+
+const remove = async () => {
+    let id = document.getElementById('d_id').value;
+
+    await $.ajax({
+        type: 'GET',
+        url: url + '/product/delete/' + id
+    }).done(res => {
+        console.log(res);
+        getProducts();
+    });
+}
+
+const getInfoUpdate = async (id) => {
+    let product = await getById(id);
+    document.getElementById('u_id').value = id;
+    document.getElementById('u_name').value = product.product[0].name;
+    document.getElementById('u_precio').value = product.product[0].precio;
+}
+
+const update = async id => {
+    let id = document.getElementById('u_id').value;
+    let name = document.getElementById('u_name').value;
+    let precio = document.getElementById('u_precio').value;
+
+    await $.ajax({
+        type: 'POST',
+        url: url + '/product/update/' + id,
+        data: {name, precio}
     }).done(res => {
         console.log(res);
     });
